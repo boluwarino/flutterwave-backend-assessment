539678af716c1036d6d008c0a09821bdb83561f7
unknown <boluwarin.oladipo@gmail.com>
2022-07-16 14:36:26 +0100

    Logic added. Load testing and deployment left.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 const app = express();
 const PORT = 5000;
 
-const payloadUno = 
+const payloadUno =
 {
     "ID": 1308,
     "Amount": 12580,
@@ -31,46 +31,76 @@ const payloadUno =
 
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("Hello FW assesor!");
 });
 
-app.post("/split-payments/compute", (req,res) => {
-    //flat compute
-    const num1 = 40;
-    const num2 = 30;
-    const result = num1 + num2;
-    res.send({
-        "ID": 1308,
-        "Balance": 0,
-        "SplitBreakdown": [
-            {
-                "SplitEntityId": "LNPYACC0019",
-                "Amount": 5000
-            },
-            {
-                "SplitEntityId": "LNPYACC0011",
-                "Amount": 2000
-            },
-            {
-                "SplitEntityId": "LNPYACC0015",
-                "Amount": 2000
+app.post("/split-payments/compute", (req, res) => {
+    const {
+        ID,
+        SplitInfo,
+        Amount
+    } = req.body;
+    const finalBreakdown = [];
+    const flatBreakdown = [];
+    const percentageBreakdown = [];
+    const ratioBreakdown = [];
+    let totalRatio = 0;
+    let currentBalance = Amount;
+
+    for (const split of SplitInfo) {
+        switch (split.SplitType) {
+            case "FLAT": {
+                flatBreakdown.push(split);
+                break;
             }
-        ]
+            case "PERCENTAGE": {
+                percentageBreakdown.push(split);
+                break;
+            }
+            case "RATIO": {
+                ratioBreakdown.push(split);
+                totalRatio += split.SplitValue;
+                break;
+            }
+            default: throw new Error();
+        }
+    }
+
+    // flat compute
+    for (const flatSplit of flatBreakdown) {
+        finalBreakdown.push({
+            SplitEntityId: flatSplit.SplitEntityId,
+            Amount: flatSplit.SplitValue
+        });
+        currentBalance -= flatSplit.SplitValue;
+    }
+
+    // percentage compute
+    for (const percentageSplit of percentageBreakdown) {
+        const value = currentBalance * (percentageSplit.SplitValue / 100);
+        finalBreakdown.push({
+            SplitEntityId: percentageSplit.SplitEntityId,
+            Amount: value
+        });
+        currentBalance -= value;
+    }
+
+    // ratio compute
+    for (const ratioSplit of ratioBreakdown) {
+        const value = currentBalance * (ratioSplit.SplitValue / totalRatio);
+        finalBreakdown.push({
+            SplitEntityId: ratioSplit.SplitEntityId,
+            Amount: value
+        });
+        currentBalance -= value;
+    }
+
+    res.send({
+        "ID": ID,
+        "Balance": currentBalance,
+        "SplitBreakdown": finalBreakdown
     });
-
-    function flatCompute() {
-        flatCalc = payloadUno.Amount + 500;
-    };
-
-    function percentageCompute() {
-        percentageCalc = payloadUno.Amount + (500*(30/100));
-    };
-
-    function ratioCompute() {
-        ratioCalc = payloadUno.Amount + 500/(payloadUno.Amount + 700);
-    };
-
 });
 
 app.listen(PORT, () => console.log(`Server running on port localhost:${PORT}`));
